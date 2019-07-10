2976127779558f1b145239e6ff5aa2d854d276a2
Daniel Rawlinson <s4204666@gillard.ace.uq.edu.au>
2019-07-10 15:39:30 +1000

    bring shell function into parallel

--- nucmer_ready_assemblies.sh
+++ nucmer_ready_assemblies.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-
+#edits contig names to include the assembly that they are sourced from
+#for use in nucmer for repeatm
 
 #remember that the assembly name is delimited from the node by __ (double underscore) this will need to go into repeatM
 module load parallel
 
 function amend_names {
 assembly=$(basename $1)
-sed  's/>/&'"$assembly"'__/' $1 > assembly_amended.$1
+echo $1 $assembly
+sed  's/>/&'"$assembly"'__/' $1 > assembly_amended."$assembly" #stored in current directory
 echo $1 amended
 }
+export -f amend_names
+
 
 ls $@ > source_assemblies.txt
 
-parallel -a source_assemblies.txt amend_names {}
+
+
+parallel --env amend_names -a source_assemblies.txt --verbose amend_names {}
 
 #previous method
 #for file in $@; do assembly=$(basename $file); sed 's/>/&'"$assembly"'__/' $file >> nucmer_ready.fa; echo $file "amended" ; done
@@ -20,3 +26,5 @@ parallel -a source_assemblies.txt amend_names {}
 wait
 
 cat *assembly_amended* >> nucmer_ready.fa
+
+module unload parallel
