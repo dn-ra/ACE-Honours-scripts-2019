cc288a3a3651c555187095142997acc07bc728e4
Daniel Rawlinson <s4204666@deakin.ace.uq.edu.au>
2019-07-10 11:52:05 +1000

    parallelised

--- nucmer_ready_assemblies.sh
+++ nucmer_ready_assemblies.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-for file in $@; do assembly=$(basename $file); sed 's/>/&'"$assembly"'__/' $file > nucmer_ready.fa; done
+
+#remember that the assembly name is delimited from the node by __ (double underscore) this will need to go into repeatM
+module load parallel
+
+function amend_names {
+assembly=$(basename $1)
+sed  's/>/&'"$assembly"'__/' $1 > assembly_amended.$1
+echo $1 amended
+}
+
+ls $@ > source_assemblies.txt
+
+parallel -a source_assemblies.txt amend_names {}
+
+#previous method
+#for file in $@; do assembly=$(basename $file); sed 's/>/&'"$assembly"'__/' $file >> nucmer_ready.fa; echo $file "amended" ; done
+
+wait
+
+cat *assembly_amended* >> nucmer_ready.fa
