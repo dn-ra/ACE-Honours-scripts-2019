e48e83c5ef4276b71ec4045de6be83446fe5da54
Daniel Rawlinson <s4204666@curtin.ace.uq.edu.au>
2019-07-20 15:03:04 +1000

    initial commit

--- map_assemblies.py
+++ map_assemblies.py
@@ -12,9 +12,18 @@ out_dir = '/srv/home/s4204666/Abisko_Assemblies/filtered_2000bp/bam_files'
 assemblies = [f for f in os.listdir(assembly_files_dir) if f.startswith('filtered.2000bp')]
 read_files = os.listdir(read_files_dir)
 read_files2 = os.listdir(read_files_2)
+completed_bams = os.listdir(out_dir)
+
 for file in assemblies:
-	read_source = read_files_dir
+	bam_check = False
 	orig_assembly = file.split(".")[2]
+	for bam in completed_bams:
+		if orig_assembly in bam:
+			bam_check = True
+			break
+	if bam_check == True:
+		continue
+	read_source = read_files_dir
 	read_couple = [f for f in read_files if orig_assembly in f]
 	if len(read_couple) == 0:
 		read_couple = [f for f in read_files2 if orig_assembly in f]
