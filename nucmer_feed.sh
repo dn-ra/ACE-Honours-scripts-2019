7eb56468b7d97cc67ed501a300342157dbcc30a1
Daniel Rawlinson <s4204666@curtin.ace.uq.edu.au>
2019-07-20 15:23:19 +1000

    ready-to-use

--- nucmer_feed.sh
+++ nucmer_feed.sh
@@ -6,21 +6,22 @@
 #input: list of n assemblies to be processed
 #ouput: n-1 .delta files to be processed by repeatM
 
-module load mummer/4.0###
+echo Script initiated at $(date)
 
-ASSEMBLIES = ${@}
+module load mummer/4.0.0beta2
 
-echo Assemblies input:
-echo $ASSEMBLIES
+read -a ASSEMBLIES <<< ${@}
 
-mkdir nucmer_out
+echo $# assemblies submited for alignment
+
+mkdir nucmer_feed_out
 touch assemblies_iter_built.fa
 
 QUERY_SIZE=0
 
 for ((i=0; i<$#-1; i++)); do
-CURR_REF=${@[$i]};
-CURR_QUE=${@[$(expr $i + 1)]};
+CURR_REF="${ASSEMBLIES["$i"]}";
+CURR_QUE="${ASSEMBLIES[$(expr $i + 1)]}";
 ITER=$(expr $i + 1);
 
 SEQ_COUNT=$(grep '>' $CURR_QUE | wc -l);
@@ -30,10 +31,9 @@ echo adding "$SEQ_COUNT" to concatenated query file in iteration "$ITER";
 cat $CURR_QUE >> assemblies_iter_built.fa;
 
 
-echo processing "$QUERY_SIZE" sequences in query file against reference file "CURR_REF";
+echo processing "$QUERY_SIZE" sequences in query file against reference file "$CURR_REF";
 
-time nucmer "$CURR_REF" assemblies_iter_built.fa -p iter_"$ITER";
+time nucmer "$CURR_REF" assemblies_iter_built.fa --delta=nucmer_feed_out/"$(date +%d%m%y)"_nucmer_iter_"$ITER" -t 24;
 done
 
 rm assemblies_iter_built.fa
-mv iter_*.delta nucmer_out
