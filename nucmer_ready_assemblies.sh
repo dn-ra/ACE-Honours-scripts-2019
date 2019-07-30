208652df83b4412fea3b5d5b6b0aa301e713f19a
DanR <rawlinson.dp@gmail.com>
2019-07-30 18:17:17 +1000

    comment edits

--- nucmer_ready_assemblies.sh
+++ nucmer_ready_assemblies.sh
@@ -3,14 +3,16 @@
 #for use in nucmer for repeatm
 
 #remember that the assembly name is delimited from the node by __ (double underscore) this will need to go into repeatM
-module load parallel
 
-#tee this so that it sends to amended source file and to concatenated nucmer_ready.fasta
+#input: list of assemblies to edit contig names for
+#output: separate assembly files, names appended, with contig names containing node-assembly links
+
+module load parallel
 
 function amend_names {
 assembly=$(basename $1)
 echo $1 $assembly
-sed  's/>/&'"$assembly"'__/' $1 > assembly_amended."$assembly" #stored in current directory
+sed  's/>/&'"$assembly"'__/' $1 > node_assembly_linked."$assembly" #stored in current directory
 echo $1 amended
 }
 export -f amend_names
@@ -18,15 +20,9 @@ export -f amend_names
 
 ls $@ > source_assemblies.txt
 
-
-
 parallel --env amend_names -a source_assemblies.txt --verbose amend_names {}
-
-#previous method
-#for file in $@; do assembly=$(basename $file); sed 's/>/&'"$assembly"'__/' $file >> nucmer_ready.fa; echo $file "amended" ; done
 
 wait
 
-cat *assembly_amended* >> nucmer_ready.fa
 
 module unload parallel
