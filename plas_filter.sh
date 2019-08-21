197e86fd1fb82487d50ac418055419f789d25f57
DanR <rawlinson.dp@gmail.com>
2019-08-21 16:38:15 +1000

    error messaging

--- plas_filter.sh
+++ plas_filter.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#first input is clustered ORF_labels file. Second input is a string used to delimit separate contig records
+
 
 TAG_FILE=$1
+
+if [ -z "$2" ]
+	then
+		echo "you must also provide a string argument that matches the beginning of the contig names (eg. \'NODE\' or \'k_141\')"
+	exit 1
+fi
 
 echo "finding all plasmid labels"
 
